08247cfc0955b27ab02556f0c2903a2d0a50cc89
anderslime <aemilnielsen@gmail.com>
2013-10-15 16:18:14 +0200

    Removed VCR
    And its helper - Webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'lokalebasen_api'
-require 'vcr'
 
 Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
 
@@ -7,11 +6,5 @@ RSpec.configure do |config|
   config.include SawyerStubs
   config.include ProviderApiFixtures
   config.include FixtureHelpers
-end
-
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/vcr'
-  c.hook_into :webmock
-  c.allow_http_connections_when_no_cassette = true
 end
 

aa778e89f44edfccc7908a0bc5a4cccdd19d252f
anderslime <aemilnielsen@gmail.com>
2013-10-17 16:21:38 +0200

    Simplifed Resource::Location spec

--- spec/lib/lokalebasen_api/resource/location_spec.rb
+++ spec/lib/lokalebasen_api/resource/location_spec.rb
@@ -7,6 +7,8 @@ describe LokalebasenApi::Resource::Location do
   let(:location_resource) {
     LokalebasenApi::Resource::Location.new(root_resource)
   }
+  let(:location_params) { { :external_key => "location_ext_key" }}
+  let(:params) { { :location => location_params }}
 
   before :each do
     stub_get(faraday_stubs, "/api/provider", 200, root_fixture)
@@ -26,14 +28,12 @@ describe LokalebasenApi::Resource::Location do
 
   it "finds a location by the external key" do
     stub_get(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
-    external_key_params = { :external_key => "location_ext_key" }
     resource = location_resource.find_by_external_key("location_ext_key")
-    resource.to_hash.should include(external_key_params)
+    resource.to_hash.should include(location_params)
     resource.should be_an_instance_of(Sawyer::Resource)
   end
 
   it "fails with NotFoundException if the no location is found with the given external key" do
-    external_key_params = { :external_key => "location_ext_key" }
     expect {
       location_resource.find_by_external_key("wrong_ext_key")
     }.to raise_error(LokalebasenApi::NotFoundException)
@@ -49,23 +49,20 @@ describe LokalebasenApi::Resource::Location do
 
   it "performs the correct requests on creation" do
     stub_post(faraday_stubs, "/api/provider/locations", 201, location_fixture)
-    params = { :location => { :external_key => "location_ext_key" } }
     location_resource.create(params)
     faraday_stubs.verify_stubbed_calls
   end
 
   it "returns a resource with location params on creation" do
     stub_post(faraday_stubs, "/api/provider/locations", 201, location_fixture)
-    params = { :location => { :external_key => "location_ext_key" } }
     location = location_resource.create(params)
-    location.to_hash.should include(params[:location])
+    location.to_hash.should include(location_params)
     location.should be_an_instance_of(Sawyer::Resource)
   end
 
   it "performs the correct requests on update" do
     stub_get(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
     stub_put(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
-    params = { :location => { :external_key => "location_ext_key"} }
     location_resource.update("location_ext_key", params)
     faraday_stubs.verify_stubbed_calls
   end
@@ -73,16 +70,14 @@ describe LokalebasenApi::Resource::Location do
   it "returns a resource with location params on update" do
     stub_get(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
     stub_put(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
-    params = { :location => { :external_key => "location_ext_key"} }
     location = location_resource.update("location_ext_key", params)
-    location.to_hash.should include(params[:location])
+    location.to_hash.should include(location_params)
     location.should be_an_instance_of(Sawyer::Resource)
   end
 
   it "performs the correct requests on deactivation" do
     stub_get(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
     stub_post(faraday_stubs, "/api/provider/locations/123/deactivations", 200, location_fixture)
-    expected_value = { :external_key => "location_ext_key" }
     location_resource.deactivate("location_ext_key")
     faraday_stubs.verify_stubbed_calls
   end
@@ -90,16 +85,14 @@ describe LokalebasenApi::Resource::Location do
   it "returns a resource with location params on deactivation" do
     stub_get(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
     stub_post(faraday_stubs, "/api/provider/locations/123/deactivations", 200, location_fixture)
-    expected_value = { :external_key => "location_ext_key" }
     return_value = location_resource.deactivate("location_ext_key")
-    return_value.to_hash.should include(expected_value)
+    return_value.to_hash.should include(location_params)
     return_value.should be_an_instance_of(Sawyer::Resource)
   end
 
   it "performs the correct requests on activation" do
     stub_get(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
     stub_post(faraday_stubs, "/api/provider/locations/123/activations", 200, location_fixture)
-    expected_value = { :external_key => "location_ext_key" }
     location_resource.activate("location_ext_key")
     faraday_stubs.verify_stubbed_calls
   end
@@ -107,9 +100,8 @@ describe LokalebasenApi::Resource::Location do
   it "returns a sawyer resource with correct params on activation" do
     stub_get(faraday_stubs, "/api/provider/locations/123", 200, location_fixture)
     stub_post(faraday_stubs, "/api/provider/locations/123/activations", 200, location_fixture)
-    expected_value = { :external_key => "location_ext_key" }
     return_value = location_resource.activate("location_ext_key")
-    return_value.to_hash.should include(expected_value)
+    return_value.to_hash.should include(location_params)
     return_value.should be_an_instance_of(Sawyer::Resource)
   end
 end

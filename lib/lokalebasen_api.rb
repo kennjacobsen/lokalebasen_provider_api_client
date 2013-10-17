978087ebda31ec5e91db43ad0fafbfe9dccc9de2
anderslime <aemilnielsen@gmail.com>
2013-10-17 11:29:53 +0200

    Use correct require order

--- lib/lokalebasen_api.rb
+++ lib/lokalebasen_api.rb
@@ -1,7 +1,4 @@
 require "lokalebasen_api/version"
-require "lokalebasen_api/client"
-require "lokalebasen_api/response_checker"
-require "lokalebasen_api/location_client"
 require "lokalebasen_api/resource/http_method_permissioning"
 require "lokalebasen_api/resource/root"
 require "lokalebasen_api/resource/location"
@@ -11,6 +8,10 @@ require "lokalebasen_api/resource/prospectus"
 require "lokalebasen_api/resource/floor_plan"
 require "lokalebasen_api/mapper/location"
 require "lokalebasen_api/mapper/job"
+require "lokalebasen_api/client_helper"
+require "lokalebasen_api/client"
+require "lokalebasen_api/response_checker"
+require "lokalebasen_api/location_client"
 
 module LokalebasenApi
   def self.client(credentials, service_url)
